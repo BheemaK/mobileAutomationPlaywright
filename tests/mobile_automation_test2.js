dc4ac4ae2d5526ad953cf68016ab575ebbf7ef4b
Bheema Kattikar <Bheema.Kattikar@appscore.com.au>
2024-01-31 09:13:37 +1100

    Modified mobile_automation_test2.js file

---
+++ tests/mobile_automation_test2.js
@@ -0,0 +1,141 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var playwright_1 = require("playwright");
+(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var browser, context, page, e_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 27, , 28]);
+                return [4 /*yield*/, playwright_1.chromium.launch({ headless: false })];
+            case 1:
+                browser = _a.sent();
+                return [4 /*yield*/, browser.newContext(playwright_1.devices['iPhone 12'])];
+            case 2:
+                context = _a.sent();
+                return [4 /*yield*/, context.newPage()];
+            case 3:
+                page = _a.sent();
+                return [4 /*yield*/, page.goto('https://www.lambdatest.com/blog/playwright-framework/')];
+            case 4:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot.png', fullPage: true })];
+            case 5:
+                _a.sent();
+                //*[@id="menu-item-81"]/a
+                //*[@id="menu-item-81"]/a
+                return [4 /*yield*/, page.locator("//*[@id=\"content\"]/div[1]/div[1]/p/a[1]").click()];
+            case 6:
+                //*[@id="menu-item-81"]/a
+                //*[@id="menu-item-81"]/a
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot1.png', fullPage: true })];
+            case 7:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"menu-item-81\"]/a").click()];
+            case 8:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot2.png', fullPage: true })];
+            case 9:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"__next\"]/div[1]/div[1]/section[1]/div/div[2]/a").click()];
+            case 10:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot3.png', fullPage: true })];
+            case 11:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"__next\"]/div[1]/div/section[1]/div[2]/a[2]").click()];
+            case 12:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot4.png', fullPage: true })];
+            case 13:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"orgname\"]").fill("Robert wharf")];
+            case 14:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot5.png', fullPage: true })];
+            case 15:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"inputLastName2\"]").fill("Rocky")];
+            case 16:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot6.png', fullPage: true })];
+            case 17:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"contctus\"]//*[@id=\"inputEmail\"]").fill("abc@abc.com")];
+            case 18:
+                _a.sent();
+                // await page.locator("/html/body/div[1]/header/nav/div/div/div[2]/div/div/div[2]/div[2]/div/div/div/form/div[2]/input"].fill("abc@abc.com");
+                //*[@id="inputEmail"]
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot7.png', fullPage: true })];
+            case 19:
+                // await page.locator("/html/body/div[1]/header/nav/div/div/div[2]/div/div/div[2]/div[2]/div/div/div/form/div[2]/input"].fill("abc@abc.com");
+                //*[@id="inputEmail"]
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"contctus\"]//*[@id=\"mobileid\"]").fill("0444222333")];
+            case 20:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot8.png', fullPage: true })];
+            case 21:
+                _a.sent();
+                return [4 /*yield*/, page.locator("//*[@id=\"contctus\"]//*[@id=\"contbtn\"]").click()];
+            case 22:
+                _a.sent();
+                return [4 /*yield*/, page.screenshot({ path: 'screenshot9.png', fullPage: true })];
+            case 23:
+                _a.sent();
+                return [4 /*yield*/, page.waitForTimeout(12000)];
+            case 24:
+                _a.sent();
+                return [4 /*yield*/, context.close()];
+            case 25:
+                _a.sent();
+                return [4 /*yield*/, browser.close()];
+            case 26:
+                _a.sent();
+                return [3 /*break*/, 28];
+            case 27:
+                e_1 = _a.sent();
+                console.error('Not able to locate elements', e_1);
+                console.log(e_1);
+                return [3 /*break*/, 28];
+            case 28: return [2 /*return*/];
+        }
+    });
+}); })();
