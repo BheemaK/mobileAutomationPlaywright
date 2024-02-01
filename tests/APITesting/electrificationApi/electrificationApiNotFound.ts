a31ba37ca49cfb93478e48eb0daab9c043fff992
Bheema Kattikar <Bheema.Kattikar@appscore.com.au>
2024-02-02 09:14:25 +1100

    Modified the file

--- tests/APITesting/electrificationApi/electrificationApiNotFound.ts
+++ tests/APITesting/electrificationApi/electrificationApiNotFound.ts
@@ -6,11 +6,13 @@ const apiUrlnewNeg = 'https://enow-bff-webap-dev.azurewebsites.net/electrificati
 (async () => {
     try {
         
+        var responseMsgNew = "Valid API post request";
         const headers = {
             'Content-Type': 'application/json'
         };
         const responseNeg = await axios.get(apiUrlnewNeg, { headers }).then((responseNeg: AxiosResponse) => {
             console.log(responseNeg.data);
+        
         })
 } catch (error) {
     console.error('Error making API request:', error);
