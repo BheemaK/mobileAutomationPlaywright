dbc172dc8ec25b261004beeb43b99593a7722517
Bheema Kattikar <Bheema.Kattikar@appscore.com.au>
2024-02-02 09:19:25 +1100

    Modified pageObject.ts file

---
+++ tests/pageObject.ts
@@ -0,0 +1,13 @@
+
+
+export const xapthLocator1 = "//*[@id=\"content\"]/div[1]/div[1]/p/a[1]";
+export const xapthLocator2 = "//*[@id=\"menu-item-81\"]/a";
+export const xapthLocator3 = "//*[@id=\"__next\"]/div[1]/div[1]/section[1]/div/div[2]/a";
+export const xapthLocator4 = "//*[@id=\"__next\"]/div[1]/div/section[1]/div[2]/a[2]";
+export const xapthLocator5 = "//*[@id=\"orgname\"]";
+export const xapthLocator6 = "//*[@id=\"inputLastName2\"]";
+export const xapthLocator7 = "//*[@id=\"contctus\"]//*[@id=\"inputEmail\"]";
+export const xapthLocator8 = "//*[@id=\"contctus\"]//*[@id=\"mobileid\"]";
+export const xapthLocator9 = "//*[@id=\"contctus\"]//*[@id=\"contbtn\"]";
+
+
